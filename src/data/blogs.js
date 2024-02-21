ac5d0f56e8a8d8a1567f085cbe45fbd6350891d3
Marco Alzamora <alzamora1008@gmail.com>
2024-02-21 03:29:37 -0500

    primera version blog

---
+++ src/data/blogs.js
@@ -0,0 +1,110 @@
+export const blogs = [
+    {
+        id: 'bocchi-1',
+        tag: 'card',
+        title: 'lorem10 ipsum dolor sit amet',
+        content: 'This is the content of card 1',
+        date: 'August 23, 2023',
+        author: 'John Doe',
+        image: 'https://noticias.animeonegai.com/wp-content/uploads/2023/03/BOcchi-the-rock-only-banner-e1679369714567.jpg'
+    },
+    {
+        id: 'bocchi-2',
+        tag: 'anime',
+        title: 'lorem10 ipsum dolor sit amet',
+        content: 'This is the content of card 2',
+        date: 'August 23, 2023',
+        author: 'John Doe',
+        image: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEho7YD0C834qKRxiSKGRZRwuINodAeDSYk28UmkAI7wZ8dQm9dBMfi__OKd-cT1Ap8IJ-bAhIGzrQ1NUvE_r0L7_yFYcwM3SwPjBJb8mudVzY5VqtQnqtLkEiRgAzbWK_-V-n586SOe1PBVj5GJtuWVFBqXmwzP06zIfJSQ3RjQgebDcbK3g7vVxzLhNg/s1600/bocchi-manga.jpg'
+    },
+    {
+        id: 'bocchi-3',
+        tag: 'card',
+        title: 'lorem10 ipsum dolor sit amet',
+        content: 'This is the content of card 3',
+        date: 'August 23, 2023',
+        author: 'John Doe',
+        image: 'https://c.wallhere.com/photos/ac/60/anime_BOCCHI_THE_ROCK-2208343.jpg!d'
+    },
+    {
+        id: 'bocchi-4',
+        tag: 'card',
+        title: 'lorem10 ipsum dolor sit amet',
+        content: 'This is the content of card 4',
+        date: 'August 23, 2023',
+        author: 'John Doe',
+        image: 'https://www.geekmi.news/__export/1691953957299/sites/debate/img/2023/08/13/geekmi_-_2023-08-13t120440_690.jpg_554688468.jpg'
+    },
+    {
+        id: 'bocchi-5',
+        tag: 'card',
+        title: 'lorem10 ipsum dolor sit amet',
+        content: 'This is the content of card 1',
+        date: 'August 23, 2023',
+        author: 'John Doe',
+        image: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiyFtr4ZljCRMeu8wCh4ucs01j0X48xcIYjygwiN79lJzkE0JuhlsqbYU1m_cDYcWg1OTx-Syvv0slSWOgAbHOa45w5xOyaky9LWJ_8AAdAaGkDjcFUXxvqFQ2U6jij0EUBFPxeXfiUhFJgTgmCqS3CgfB8BKdLfXVbk6McqYKe5PnDwnYqe3viKv5EDg/s16000/mj0.webp'
+    },
+    // {
+    //     id: 2,
+    //     tag: 'card',
+    //     title: 'lorem10 ipsum dolor sit amet',
+    //     content: 'This is the content of card 2',
+    //     date: 'August 23, 2023',
+    //     author: 'John Doe',
+    //     image: 'https://noticias.animeonegai.com/wp-content/uploads/2023/03/BOcchi-the-rock-only-banner-e1679369714567.jpg'
+    // },
+    // {
+    //     id: 3,
+    //     tag: 'card',
+    //     title: 'lorem10 ipsum dolor sit amet',
+    //     content: 'This is the content of card 3',
+    //     date: 'August 23, 2023',
+    //     author: 'John Doe',
+    //     image: 'https://noticias.animeonegai.com/wp-content/uploads/2023/03/BOcchi-the-rock-only-banner-e1679369714567.jpg'
+    // },
+    // {
+    //     id: 4,
+    //     tag: 'card',
+    //     title: 'lorem10 ipsum dolor sit amet',
+    //     content: 'This is the content of card 4',
+    //     date: 'August 23, 2023',
+    //     author: 'John Doe',
+    //     image: 'https://noticias.animeonegai.com/wp-content/uploads/2023/03/BOcchi-the-rock-only-banner-e1679369714567.jpg'
+    // },
+    // {
+    //     id: 1,
+    //     tag: 'card',
+    //     title: 'lorem10 ipsum dolor sit amet',
+    //     content: 'This is the content of card 1',
+    //     date: 'August 23, 2023',
+    //     author: 'John Doe',
+    //     image: 'https://noticias.animeonegai.com/wp-content/uploads/2023/03/BOcchi-the-rock-only-banner-e1679369714567.jpg'
+    // },
+    // {
+    //     id: 2,
+    //     tag: 'card',
+    //     title: 'lorem10 ipsum dolor sit amet',
+    //     content: 'This is the content of card 2',
+    //     date: 'August 23, 2023',
+    //     author: 'John Doe',
+    //     image: 'https://noticias.animeonegai.com/wp-content/uploads/2023/03/BOcchi-the-rock-only-banner-e1679369714567.jpg'
+    // },
+    // {
+    //     id: 3,
+    //     tag: 'card',
+    //     title: 'lorem10 ipsum dolor sit amet',
+    //     content: 'This is the content of card 3',
+    //     date: 'August 23, 2023',
+    //     author: 'John Doe',
+    //     image: 'https://noticias.animeonegai.com/wp-content/uploads/2023/03/BOcchi-the-rock-only-banner-e1679369714567.jpg'
+    // },
+    // {
+    //     id: 4,
+    //     tag: 'card',
+    //     title: 'lorem10 ipsum dolor sit amet',
+    //     content: 'This is the content of card 4',
+    //     date: 'August 23, 2023',
+    //     author: 'John Doe',
+    //     image: 'https://noticias.animeonegai.com/wp-content/uploads/2023/03/BOcchi-the-rock-only-banner-e1679369714567.jpg'
+    // },
+]
