e27649d60e3336330da7bad0cf46cfd8c7d67755
Marco Alzamora <alzamora1008@gmail.com>
2024-03-06 20:20:20 -0500

    testing: botones de redes sociales para compartir articulos

--- src/data/blogs.js
+++ src/data/blogs.js
@@ -6,7 +6,30 @@ export const blogs = [
         content: 'This is the content of card 1',
         date: 'August 23, 2023',
         author: 'John Doe',
-        image: 'https://noticias.animeonegai.com/wp-content/uploads/2023/03/BOcchi-the-rock-only-banner-e1679369714567.jpg'
+        image: '/public/images/boccho.jpg',
+        sections: [
+            {
+                subtitle: 'Section 1',
+                text: 'This is the content of section 1',
+                image: 'https://noticias.animeonegai.com/wp-content/uploads/2023/03/BOcchi-the-rock-only-banner-e1679369714567.jpg'
+            },
+            {
+                subtitle: 'Section 11',
+                text: 'This is the content of section 1',
+                image: ''
+            },
+            {
+                subtitle: 'Section 21',
+                text: 'This is the content of section 1',
+                image: 'https://noticias.animeonegai.com/wp-content/uploads/2023/03/BOcchi-the-rock-only-banner-e1679369714567.jpg'
+            },
+            {
+                subtitle: 'Section 431',
+                text: 'This is the content of section 1',
+                image: ''
+            },
+        ]
+
     },
     {
         id: 'bocchi-2',
@@ -15,7 +38,29 @@ export const blogs = [
         content: 'This is the content of card 2',
         date: 'August 23, 2023',
         author: 'John Doe',
-        image: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEho7YD0C834qKRxiSKGRZRwuINodAeDSYk28UmkAI7wZ8dQm9dBMfi__OKd-cT1Ap8IJ-bAhIGzrQ1NUvE_r0L7_yFYcwM3SwPjBJb8mudVzY5VqtQnqtLkEiRgAzbWK_-V-n586SOe1PBVj5GJtuWVFBqXmwzP06zIfJSQ3RjQgebDcbK3g7vVxzLhNg/s1600/bocchi-manga.jpg'
+        image: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEho7YD0C834qKRxiSKGRZRwuINodAeDSYk28UmkAI7wZ8dQm9dBMfi__OKd-cT1Ap8IJ-bAhIGzrQ1NUvE_r0L7_yFYcwM3SwPjBJb8mudVzY5VqtQnqtLkEiRgAzbWK_-V-n586SOe1PBVj5GJtuWVFBqXmwzP06zIfJSQ3RjQgebDcbK3g7vVxzLhNg/s1600/bocchi-manga.jpg',
+        sections: [
+            {
+                subtitle: 'Section 0',
+                text: 'This is the content of section 1',
+                image: 'https://noticias.animeonegai.com/wp-content/uploads/2023/03/BOcchi-the-rock-only-banner-e1679369714567.jpg'
+            },
+            {
+                subtitle: 'Section 2',
+                text: 'This is the content of section 1',
+                image: ''
+            },
+            {
+                subtitle: 'Section 10',
+                text: 'This is the content of section 1',
+                image: 'https://noticias.animeonegai.com/wp-content/uploads/2023/03/BOcchi-the-rock-only-banner-e1679369714567.jpg'
+            },
+            {
+                subtitle: 'Section 5',
+                text: 'This is the content of section 1',
+                image: ''
+            },
+        ]
     },
     {
         id: 'bocchi-3',
@@ -24,7 +69,29 @@ export const blogs = [
         content: 'This is the content of card 3',
         date: 'August 23, 2023',
         author: 'John Doe',
-        image: 'https://c.wallhere.com/photos/ac/60/anime_BOCCHI_THE_ROCK-2208343.jpg!d'
+        image: 'https://c.wallhere.com/photos/ac/60/anime_BOCCHI_THE_ROCK-2208343.jpg!d',
+        sections: [
+            {
+                subtitle: 'Section 1',
+                text: 'This is the content of section 1',
+                image: 'https://noticias.animeonegai.com/wp-content/uploads/2023/03/BOcchi-the-rock-only-banner-e1679369714567.jpg'
+            },
+            {
+                subtitle: 'Section 2',
+                text: 'This is the content of section 1',
+                image: ''
+            },
+            {
+                subtitle: 'Section 3',
+                text: 'This is the content of section 1',
+                image: 'https://noticias.animeonegai.com/wp-content/uploads/2023/03/BOcchi-the-rock-only-banner-e1679369714567.jpg'
+            },
+            {
+                subtitle: 'Section 4',
+                text: 'This is the content of section 1',
+                image: ''
+            },
+        ]
     },
     {
         id: 'bocchi-4',
@@ -33,7 +100,29 @@ export const blogs = [
         content: 'This is the content of card 4',
         date: 'August 23, 2023',
         author: 'John Doe',
-        image: 'https://www.geekmi.news/__export/1691953957299/sites/debate/img/2023/08/13/geekmi_-_2023-08-13t120440_690.jpg_554688468.jpg'
+        image: 'https://www.geekmi.news/__export/1691953957299/sites/debate/img/2023/08/13/geekmi_-_2023-08-13t120440_690.jpg_554688468.jpg',
+        sections: [
+            {
+                subtitle: 'Section 1',
+                text: 'This is the content of section 1',
+                image: 'https://noticias.animeonegai.com/wp-content/uploads/2023/03/BOcchi-the-rock-only-banner-e1679369714567.jpg'
+            },
+            {
+                subtitle: 'Section 2',
+                text: 'This is the content of section 1',
+                image: ''
+            },
+            {
+                subtitle: 'Section 3',
+                text: 'This is the content of section 1',
+                image: 'https://noticias.animeonegai.com/wp-content/uploads/2023/03/BOcchi-the-rock-only-banner-e1679369714567.jpg'
+            },
+            {
+                subtitle: 'Section 4',
+                text: 'This is the content of section 1',
+                image: ''
+            },
+        ]
     },
     {
         id: 'bocchi-5',
@@ -42,7 +131,29 @@ export const blogs = [
         content: 'This is the content of card 1',
         date: 'August 23, 2023',
         author: 'John Doe',
-        image: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiyFtr4ZljCRMeu8wCh4ucs01j0X48xcIYjygwiN79lJzkE0JuhlsqbYU1m_cDYcWg1OTx-Syvv0slSWOgAbHOa45w5xOyaky9LWJ_8AAdAaGkDjcFUXxvqFQ2U6jij0EUBFPxeXfiUhFJgTgmCqS3CgfB8BKdLfXVbk6McqYKe5PnDwnYqe3viKv5EDg/s16000/mj0.webp'
+        image: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiyFtr4ZljCRMeu8wCh4ucs01j0X48xcIYjygwiN79lJzkE0JuhlsqbYU1m_cDYcWg1OTx-Syvv0slSWOgAbHOa45w5xOyaky9LWJ_8AAdAaGkDjcFUXxvqFQ2U6jij0EUBFPxeXfiUhFJgTgmCqS3CgfB8BKdLfXVbk6McqYKe5PnDwnYqe3viKv5EDg/s16000/mj0.webp',
+        sections: [
+            {
+                subtitle: 'Section 1',
+                text: 'This is the content of section 1',
+                image: 'https://noticias.animeonegai.com/wp-content/uploads/2023/03/BOcchi-the-rock-only-banner-e1679369714567.jpg'
+            },
+            {
+                subtitle: 'Section 1',
+                text: 'This is the content of section 1',
+                image: ''
+            },
+            {
+                subtitle: 'Section 1',
+                text: 'This is the content of section 1',
+                image: 'https://noticias.animeonegai.com/wp-content/uploads/2023/03/BOcchi-the-rock-only-banner-e1679369714567.jpg'
+            },
+            {
+                subtitle: 'Section 1',
+                text: 'This is the content of section 1',
+                image: ''
+            },
+        ]
     },
     // {
     //     id: 2,
