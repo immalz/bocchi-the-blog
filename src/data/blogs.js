2146f3c673de8dacc86277938bd69718765c386b
Marco Alzamora <alzamora1008@gmail.com>
2024-03-07 23:08:17 -0500

    first real article

--- src/data/blogs.js
+++ src/data/blogs.js
@@ -1,32 +1,42 @@
 export const blogs = [
     {
-        id: 'bocchi-1',
-        tag: 'card',
-        title: 'lorem10 ipsum dolor sit amet',
-        content: 'This is the content of card 1',
-        date: 'August 23, 2023',
-        author: 'John Doe',
-        image: '/public/images/boccho.jpg',
+        id: 'fallece-akira-toriyama-a-la-edad-de-68',
+        tag: 'anime',
+        title: 'Fallece Akira Toriyama a los 68 años, el padre de Dragon Ball',
+        content: 'Estamos muy tristes de compartir que el creador de manga Akira Toriyama falleció el 1 de marzo debido a un hematoma subdural. Tenía 68 años...',
+        date: 'Marzo 07, 2024',
+        author: 'Marco Alzamora Cueto',
+        image: 'https://cdn.milenio.com/uploads/media/2024/03/07/muere-akira-toriyama.jpg',
         sections: [
             {
-                subtitle: 'Section 1',
-                text: 'This is the content of section 1',
-                image: 'https://noticias.animeonegai.com/wp-content/uploads/2023/03/BOcchi-the-rock-only-banner-e1679369714567.jpg'
-            },
-            {
-                subtitle: 'Section 11',
-                text: 'This is the content of section 1',
+                subtitle: '',
+                text: 'El mundo del entretenimiento se encuentra de luto ya que la tarde de este jueves 7 de marzo se dio a conocer la noticia de la muerte de Akira Toriyama, mejor conocido por ser el creador del muy famoso anime Dragon Ball.',
                 image: ''
             },
             {
-                subtitle: 'Section 21',
-                text: 'This is the content of section 1',
-                image: 'https://noticias.animeonegai.com/wp-content/uploads/2023/03/BOcchi-the-rock-only-banner-e1679369714567.jpg'
+                subtitle: '',
+                text: 'Fue a través de redes sociales en donde varias páginas de anime comenzaron a confirmar la noticia. Asimismo, la cuenta oficial de Dragon Ball en X, antes Twitter, emitió un comunicado en donde informaba que el mangaka murió el pasado primero de marzo.',
+                image: 'https://i.blogs.es/037da8/comunicado-muerte-akirta-toriyama-creador-dragon-ball-goku/1366_2000.jpeg'
             },
             {
-                subtitle: 'Section 431',
-                text: 'This is the content of section 1',
+                subtitle: '',
+                text: 'El comunicado incluso menciona que esta triste noticia se da en plena producción de este nuevo anime. Su funeral fue con sus familiares y gente cercana al creativo, "siguiendo sus deseos por tranquilidad". También se menciona que por ahora no hay nada planeado para rendirle un tributo, pero que se comunicará cualquier decisión oficial próximamente.',
                 image: ''
+            },
+            {
+                subtitle: '¿De qué murió?',
+                text: 'De acuerdo con el comunicado oficial el creador del anime murió debido a un hematoma subdural agudo, que es a menudo el resultado de un traumatismo craneal grave. Este tipo de hematoma subdural se encuentra entre el más letal de todos los traumatismos craneales.',
+                image: ''
+            },
+            {
+                subtitle: '¿Quién fue Arika Toriyama?',
+                text: 'Akira Toriyama fue un mangaka y diseñador de personajes japonés. Era conocido por su obra maestra Dragon Ball, y actuando como diseñador de personajes para varios videojuegos populares como la saga de Dragon Quest, Chrono Trigger y Blue Dragon.',
+                image: ''
+            },
+            {
+                subtitle: '',
+                text: 'Toriyama era considerado como uno de los artistas que cambió la historia del manga, ya que sus obras son muy influyentes y populares, particularmente la serie de Gokú y compañia y la saga Dragon Quest.',
+                image: 'https://www.blognone.com/sites/default/files/externals/dbc9f6b8f68a491614f953eb85b5effe.jpg'
             },
         ]
 
