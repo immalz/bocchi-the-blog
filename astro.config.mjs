fa11ea89a610e4f0669604fbaf1a101d9077ccc5
Marco Alzamora <alzamora1008@gmail.com>
2024-03-31 14:05:35 -0500

    fixing SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap"
+import { VitePWA } from "vite-plugin-pwa"
 
 import vercel from "@astrojs/vercel/serverless";
 import { seoConfig, manifest } from './src/utils/seoConfig';
 
 export default defineConfig({
+	compressHTML: true,
 	prefetch: true,
 	devToolbar: {
 		enabled: false,
@@ -15,51 +17,54 @@ export default defineConfig({
 		},
 	}),
 	integrations: [sitemap()],
-	output: "hybrid",
+	build: {
+		inlineStylesheets: "always",
+	},
+	output: "server",
 	site: seoConfig.baseURL,
-	// vite: {
-	// 	build: {
-	// 		cssMinify: "lightningcss",
-	// 	},
-	// 	ssr: {
-	// 		noExternal: ["path-to-regexp"],
-	// 	},
-	// 	plugins: [
-	// 		VitePWA({
-	// 			registerType: "autoUpdate",
-	// 			manifest,
-	// 			workbox: {
-	// 				globDirectory: ".vercel/output/static",
-	// 				globPatterns: [
-	// 					"**/*.{html,js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico}"
-	// 				],
-	// 				runtimeCaching: [
-	// 					{
-	// 						urlPattern: /\.(?:png|jpg|jpeg|svg|gif|webp)$/,
-	// 						handler: "CacheFirst",
-	// 						options: {
-	// 							cacheName: "images",
-	// 							expiration: {
-	// 								maxEntries: 50,
-	// 								maxAgeSeconds: 30 * 24 * 60 * 60,
-	// 							},
-	// 						},
-	// 					},
-	// 					{
-	// 						urlPattern: /^https?.*/,
-	// 						handler: "StaleWhileRevalidate",
-	// 						options: {
-	// 							cacheName: "static-assets",
-	// 							expiration: {
-	// 								maxEntries: 200,
-	// 								maxAgeSeconds: 24 * 60 * 60 * 30,
-	// 							},
-	// 						},
-	// 					},
-	// 				],
-	// 				navigateFallback: null,
-	// 			}
-	// 		}),
-	// 	],
-	// },
+	vite: {
+		build: {
+			cssMinify: "lightningcss",
+		},
+		ssr: {
+			noExternal: ["path-to-regexp"],
+		},
+		plugins: [
+			VitePWA({
+				registerType: "autoUpdate",
+				manifest,
+				workbox: {
+					globDirectory: ".vercel/output/static",
+					globPatterns: [
+						"**/*.{html,js,css,svg,avif,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico}",
+					],
+					runtimeCaching: [
+						{
+							urlPattern: /\.(?:png|jpg|jpeg|svg|gif|webp|avif)$/,
+							handler: "CacheFirst",
+							options: {
+								cacheName: "images",
+								expiration: {
+									maxEntries: 100,
+									maxAgeSeconds: 30 * 24 * 60 * 60,
+								},
+							},
+						},
+						{
+							urlPattern: /\.(?:woff|woff2|ttf|eot|ico)$/,
+							handler: "CacheFirst",
+							options: {
+								cacheName: "fonts",
+								expiration: {
+									maxEntries: 10,
+									maxAgeSeconds: 30 * 24 * 60 * 60,
+								},
+							},
+						},
+					],
+					navigateFallback: null,
+				},
+			}),
+		],
+	},
 });
